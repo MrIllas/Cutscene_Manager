79b1b13cc9d27ff49fc18403fc88f51e87c193d6
MrIllas <44169046+MrIllas@users.noreply.github.com>
2022-05-03 23:51:05 +0200

    Cleaning

--- Game/Assets/Maps/buildings.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="buildings" tilewidth="16" tileheight="16" tilecount="6231" columns="93">
- <image source="../Sprites/Buildings/buildings.png" width="1503" height="1072"/>
-</tileset>
